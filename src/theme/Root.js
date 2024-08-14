5b811b80e6849b849a151d6d2f0eb7d01a7ab28f
Krithika <76915822+krithikasb@users.noreply.github.com>
2024-08-14 14:26:22 -0700

    preload iframe in non-iframe pages (#66)

---
+++ src/theme/Root.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import Iframe from "@site/src/components/Iframe";
+import DEFAULT_APP_CODE from "@site/src/app-iframe/python/default.py";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+
+// Swizzling (wrapping) the Root component since it never unmounts
+// This wrapper mounts MAX_IFRAMES_PER_PAGE iframes on the page
+// so that when we go to a page with visible iframes, it's already loaded
+
+// Modify MAX_IFRAMES_PER_PAGE to the number of iframes that need to be preloaded
+// Add PATHS_WITH_THREE_IFRAMES etc. if needed
+// Modify checkIfPathContainsIframe function for additional iframes
+
+const PATHS_WITH_ONE_IFRAME = [
+  "/basics/transform/geospatial/buffer/",
+  "/basics/transform/geospatial/point-in-polygon/",
+  "/basics/transform/geospatial/raster-h3/",
+  "/basics/transform/geospatial/zonal_stats/",
+];
+
+const PATHS_WITH_TWO_IFRAMES = [];
+
+const MAX_IFRAMES_PER_PAGE = 2;
+
+function checkIfPathContainsIframe(index, path) {
+  if (index <= 1) {
+    return PATHS_WITH_ONE_IFRAME.includes(path);
+  }
+  if (index <= 2) {
+    return PATHS_WITH_TWO_IFRAMES.includes(path);
+  }
+}
+
+function RootIframeComponent() {
+  const [iframesHaveLoaded, setIframesHaveLoaded] = useState(false);
+
+  useEffect(() => {
+    setIframesHaveLoaded(true);
+  });
+
+  function currentPathContainsIframe(index) {
+    if (!ExecutionEnvironment.canUseDOM) {
+      return true;
+    }
+    checkIfPathContainsIframe(index, window.location.pathname);
+  }
+
+  return (
+    <>
+      {iframesHaveLoaded
+        ? null
+        : Array.from({ length: MAX_IFRAMES_PER_PAGE }).map((_, index) => {
+            return currentPathContainsIframe(index + 1) ? null : (
+              <Iframe
+                key={index + 1}
+                id={`iframe-${index + 1}`}
+                code={DEFAULT_APP_CODE}
+                requirements={[
+                  "/pyarrow/pyarrow-17.0.0-cp312-cp312-pyodide_2024_0_wasm32.whl",
+                  "micropip",
+                  "pyodide-unix-timezones", // needed by pyarrow
+                  "geopandas",
+                  "requests",
+                  "xarray",
+                  "yarl",
+                  // Commonly used in product:
+                  "pydeck",
+                ]}
+                visible={false}
+              />
+            );
+          })}
+    </>
+  );
+}
+
+export default function Root({ children }) {
+  return (
+    <>
+      {children}
+      <BrowserOnly>
+        {() => {
+          return <RootIframeComponent />;
+        }}
+      </BrowserOnly>
+    </>
+  );
+}
