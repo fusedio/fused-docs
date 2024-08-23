bc39ba71ffb571a755aa160f37f8fbc48f755a93
Plinio Guzman <pgzmnk@gmail.com>
2024-08-23 11:08:44 -0600

    staging (#77)
    * update images and animations
    
    * update urls and filenames
    
    * update requirements_fused.txt
    
    * update udf links
    
    * update comb

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MDXComponents from "@theme-original/MDXComponents";
+import ReactPlayer from "react-player";
+import Tag from "@site/src/components/Tag";
+
+export default {
+  ...MDXComponents,
+  ReactPlayer,
+  Tag,
+};
