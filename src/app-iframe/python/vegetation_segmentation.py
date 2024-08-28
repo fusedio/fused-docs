6e5cb4f491e1482696f74960a2c3b5456054dc33
Kevin Lacaille <kevinlacaille@gmail.com>
2024-08-28 16:21:17 -0700

    added video, updated demo

--- src/app-iframe/python/vegetation_segmentation.py
+++ src/app-iframe/python/vegetation_segmentation.py
@@ -1,22 +1,20 @@
 import streamlit as st
 # Sidebar sliders for parameters
-st.header("Model Parameters")
+# st.header("Model Parameters")
 
 available_indices = [
     'VARI: Visible Atmospherically Resistant Index',  # Visible Atmospherically Resistant Index
     'GLI: Green Leaf Index',  # Green Leaf Index
-    'RGRI: Red-Green Ratio Index',  # Red-Green Ratio Index
-    'ExG: Excess Green Index',  # Excess Green Index
-    'CIVE: Color Index of Vegetation Extraction',  # Color Index of Vegetation Extraction
-    'VIgreen: Vegetative Index (VIgreen)',  # Vegetative Index (VIgreen)
-    'NGRDI: Normalized Green-Red Difference Index'  # Normalized Green-Red Difference Index
+    'RGRI: Red-Green Ratio Index (>0.9 for buildings)',  # Red-Green Ratio Index
+    # 'ExG: Excess Green Index',     # Excess Green Index
+    # 'NGRDI: Normalized Green-Red Difference Index',    # Normalized Green-Red Difference Index
 ]
 
 selected_name = st.selectbox("Select Vegetation Index", available_indices)
 index_method = available_indices.index(selected_name)
 
 # Slider for morphological kernel size
-index_min, index_max = st.slider("Morphological Kernel Size (odd number)",
+index_min, index_max = st.slider("Thresholding Index Values",
                                  min_value=0.0,
                                  max_value=1.0,
                                  value=(0.1, 0.9),
@@ -61,15 +59,15 @@ st.components.v1.html(f"""<!DOCTYPE html>
         //   "pk.eyJ1IjoiZXN0b3JudWRhbWUiLCJhIjoiY2xneTh0Y3czMDczODNmcG11ZTNuazZvbSJ9.QFTdgqDlAFQKaJ_QLA35ew",
         // mapStyle: "mapbox://styles/mapbox/dark-v10",
         initialViewState: {{
-          longitude: -122.7957,
-          latitude: 49.2898,
-          zoom: 17,
+          longitude: -122.837838,
+          latitude: 49.291155,
+          zoom: 16,
         }},
         controller: true,
         layers: [
           new TileLayer({{
             id: "base",
-            data: "https://staging.fused.io/server/v1/realtime-shared/fsh_7MDUq8kscLii9MhiTvuU8o/run/tiles/{{z}}/{{x}}/{{y}}?dtype_out_raster=png&index_min={index_min}&index_max={index_max}&index_method={index_method}&return_mask={False}",
+            data: "https://www.fused.io/server/v1/realtime-shared/fsh_2YDT4gAuNEasD4CWu6Mave/run/tiles/{{z}}/{{x}}/{{y}}?dtype_out_raster=png&index_min={index_min}&index_max={index_max}&index_method={index_method}&return_mask=False",
             maxZoom: 25,
             minZoom: 0,
 
@@ -85,7 +83,7 @@ st.components.v1.html(f"""<!DOCTYPE html>
           }}),
           new TileLayer({{
             id: "data",
-            data: "https://staging.fused.io/server/v1/realtime-shared/fsh_7MDUq8kscLii9MhiTvuU8o/run/tiles/{{z}}/{{x}}/{{y}}?dtype_out_raster=png&index_min={index_min}&index_max={index_max}&index_method={index_method}&return_mask={mask}",
+            data: "https://www.fused.io/server/v1/realtime-shared/fsh_2YDT4gAuNEasD4CWu6Mave/run/tiles/{{z}}/{{x}}/{{y}}?dtype_out_raster=png&index_min={index_min}&index_max={index_max}&index_method={index_method}&return_mask={mask}",
             maxZoom: 25,
             minZoom: 0,
 
@@ -105,4 +103,4 @@ st.components.v1.html(f"""<!DOCTYPE html>
   </body>
 </html>
 """,
-                      height=500)
+                      height=600)
