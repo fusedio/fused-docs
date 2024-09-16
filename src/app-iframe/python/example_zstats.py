7f50ef0c7f3b476f2e51da684c8111ab992bc0c5
pgzmnk <pgzmnk@gmail.com>
2024-09-16 10:11:09 -0600

    stats update v2

--- src/app-iframe/python/example_zstats.py
+++ src/app-iframe/python/example_zstats.py
@@ -1,26 +1,37 @@
 import pydeck as pdk
 import streamlit as st
 
-url_overture = "https://www.fused.io/server/v1/realtime-shared/fsh_6eYdGuG95JPEmUPaHu2CQX/run/tiles/{z}/{x}/{y}?dtype_out_vector=geojson"
+st.title("🌽 CDL Zonal Stats")
+year = st.selectbox("Year", [2020, 2021, 2022])
+
+url = (
+    "https://www.fused.io/server/v1/realtime-shared/fsh_46eSFZaR3q3SnoVB28pN0g/run/tiles/{z}/{x}/{y}?dtype_out_vector=geojson"
+    + f"&year={year}"
+)
 st.pydeck_chart(
     pdk.Deck(
         map_style="mapbox://styles/mapbox/dark-v9",
-        initial_view_state=pdk.ViewState(latitude=42.2027, longitude=-121.6551, zoom=6),
+        initial_view_state=pdk.ViewState(latitude=40.3612, longitude=-111.5386, zoom=6),
         layers=[
             pdk.Layer(
                 "TileLayer",
-                data=url_overture,
-                get_line_color=[255, 25, 2, 100],
+                data=url,
+                get_line_color=[255, 255, 255, 100],
                 stroked=True,
-                get_line_width=2,
+                line_width_min_pixels=1,
                 pickable=True,
                 filled=True,
-                get_fill_color=["properties.stats / 10", 10, 140],
+                get_fill_color=[
+                    "properties.stats * 200",
+                    "properties.stats * 100",
+                    10,
+                    100,
+                ],
             )
         ],
         tooltip={
-            "html": "<b>Average elevation:</b> {stats} <br/> meters. ",
+            "html": "<b>Stats:</b> {stats} <br/> ",
             "style": {"backgroundColor": "steelblue", "color": "white"},
         },
-    ),
+    )
 )
