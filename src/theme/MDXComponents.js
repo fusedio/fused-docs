4d5e35c8404f43c283cd7f18939d3ce287137243
pgzmnk <pgzmnk@gmail.com>
2024-09-16 17:51:57 -0600

    update broken images with idealimage

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,9 +2,11 @@ import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
 import ReactPlayer from "react-player";
 import Tag from "@site/src/components/Tag";
+import Image from "@theme/IdealImage";
 
 export default {
   ...MDXComponents,
   ReactPlayer,
   Tag,
+  Image,
 };
