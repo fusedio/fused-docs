aa02bfaa01bd21c89eb73c72c291cd6c473f78ac
Plinio Guzman <pgzmnk@gmail.com>
2024-09-16 19:22:08 -0600

    Revert "staging 3 prior to hard reset (#148)" (#149)
    This reverts commit 9a686578c19f86adb43692acdb7da928c4a7ee57.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -229,16 +229,6 @@ const config: Config = {
         anonymizeIP: true,
       },
     ],
-    [
-      "@docusaurus/plugin-ideal-image",
-      {
-        quality: 70,
-        max: 1030, // max resized image's size.
-        min: 640, // min resized image's size. if original is lower, use that size.
-        steps: 2, // the max number of images generated between min and max (inclusive)
-        disableInDev: false,
-      },
-    ],
   ],
 };
 
