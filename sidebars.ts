dba7911f58fd5140233370cf26e085eb2616223f
Dusan Rajkovic <dusan@fused.io>
2025-02-10 12:10:39 +0100

    Contribute to Fused moved under UDF Catalog page

--- sidebars.ts
+++ sidebars.ts
@@ -105,7 +105,6 @@ const sidebars: SidebarsConfig = {
         "python-sdk/authentication",
         "python-sdk/dependencies",
         "python-sdk/changelog",
-        "python-sdk/contribute",
       ],
     },
     {
