a18e2dddf3e4c450f3f63496cd6a3cdfe0e854d6
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-08-11 15:42:19 +0200

    run_remote -> run_batch

--- utils/generate_reference_docs.py
+++ utils/generate_reference_docs.py
@@ -58,10 +58,10 @@ toc_max_heading_level: 4
 """
 
 run_remote_addition = """
-#### `job.run_remote`
+#### `job.run_batch`
 
 ```python showLineNumbers
-def run_remote(output_table: Optional[str] = ...,
+def run_batch(output_table: Optional[str] = ...,
     instance_type: Optional[WHITELISTED_INSTANCE_TYPES] = None,
     *,
     region: str | None = None,
@@ -75,7 +75,7 @@ def run_remote(output_table: Optional[str] = ...,
     overwrite: Optional[bool] = None) -> RunResponse
 ```
 
-Begin execution of the ingestion job by calling `run_remote` on the job object.
+Begin execution of the ingestion job by calling `run_batch` on the job object.
 
 **Arguments**:
 
@@ -90,7 +90,7 @@ Begin execution of the ingestion job by calling `run_remote` on the job object.
 
 #### Monitor and manage job
 
-Calling `run_remote` returns a `RunResponse` object with helper methods.
+Calling `run_batch` returns a `RunResponse` object with helper methods.
 
 ```python showLineNumbers
 # Declare ingest job
@@ -100,7 +100,7 @@ job = fused.ingest(
 )
 
 # Start ingest job
-job_id = job.run_remote()
+job_id = job.run_batch()
 ```
 
 Fetch the job status.
