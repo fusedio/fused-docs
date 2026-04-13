8636ccfeee19303abf2d7df720e8d5a64c5b3f4e
Aman Bagrecha <76432265+amanbagrecha@users.noreply.github.com>
2026-04-13 13:07:28 +0530

    docs: remove Currency Prediction example (#1016)

--- sidebars.ts
+++ sidebars.ts
@@ -165,7 +165,6 @@ const sidebars: SidebarsConfig = {
         "examples/climate-dashboard",
         "examples/maxar-satellite-imagery",
         "examples/site-selection-analysis",
-        "examples/currency-prediction",
       ],
     },
 
